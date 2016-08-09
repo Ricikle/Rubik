9fc17b28a8ca6bf9484647e8775deef4cc2b1b58
Alex Rice <alexrice999@hotmail.co.uk>
2016-08-09 23:41:44 +0100

    Bug Fix

--- Stage1.hs
+++ Stage1.hs
@@ -8,7 +8,12 @@ module Stage1 where
   import Control.Monad
 
   main :: IO ()
-  main = putStrLn $ show $ generateTable
+  main = putStrLn $ show $ getMoveList (generateTable) (edgeO(apply [U,B,R,U,U2,F,L] identity))
+
+  getMoveList :: Map.Map Orientation Move -> Orientation -> [Move]
+  getMoveList ma o = if o == fromList [0,0,0,0,0,0,0,0,0,0,0,0]
+    then [] else let m = ma Map.! o in m : getMoveList ma (moveOrientationEdge o m)
+
 
   generateTable :: Map.Map Orientation Move
   generateTable = execState (bfs (S.singleton identity)) Map.empty where
@@ -18,6 +23,7 @@ module Stage1 where
       ys <- forM moves $ \m -> do
         let c = apply [m] x
         ma <- get
-        if Map.member (edgeO c) ma then
-          modify ( Map.insert (edgeO c) m) >> return [c] else return []
+        if Map.member (edgeO c) ma
+          then return []
+          else modify ( Map.insert (edgeO c) (invert m)) >> return [c]
       bfs (xs S.>< S.fromList (concat ys) )
