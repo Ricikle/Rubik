5f924cfd03bc0b568c560e22ae9a6de4e245676b
Alex Rice <alexrice999@hotmail.co.uk>
2016-08-12 12:21:52 +0100

    Write main

--- Main.hs
+++ Main.hs
@@ -1,4 +1,29 @@
 module Main where
+  import System.Directory
+  import qualified Stage1 as Stage1
+  import qualified Stage2 as Stage2
+  import qualified Stage3 as Stage3
+  import qualified Stage4 as Stage4
 
-main :: IO ()
-main = putStrLn "main file not configured yet!"
+
+  checkFile :: FilePath -> IO Bool
+  checkFile fp = do
+    b <- doesFileExist fp
+    if b then do
+      putStrLn $ "Found " ++ fp
+      r <- readFile fp
+      if not . null $ r then
+        return True
+        else (putStrLn $ fp ++ " is empty") >> return False
+      else putStrLn (fp ++ " not found") >> return False
+
+  main :: IO ()
+  main = do
+    s1 <- checkFile "Stage1.dat"
+    s2 <- checkFile "Stage2.dat"
+    s3 <- checkFile "Stage3.dat"
+    s4 <- checkFile "Stage4.dat"
+    if s1 then return () else putStrLn "Writing Table for Stage 1" >> Stage1.writeTable
+    if s2 then return () else putStrLn "Writing Table for Stage 2" >> Stage2.writeTable 
+    if s3 then return () else putStrLn "Writing Table for Stage 3" >> Stage3.writeTable
+    if s4 then return () else putStrLn "Writing Table for Stage 4" >> Stage4.writeTable
